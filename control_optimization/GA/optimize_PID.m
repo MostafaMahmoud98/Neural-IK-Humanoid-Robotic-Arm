8a376084853f133eba2a7f33bcc21c9ce818d0ce
Mustafa Mahmoud Elsayed <mostafamahmoud762@gmail.com>
2026-05-07 19:26:41 +0300

    codex

--- control_optimization/GA/optimize_PID.m
+++ control_optimization/GA/optimize_PID.m
@@ -1,6 +1,29 @@
-
 function cost = optimize_PID(k)
-assignin('base','k',k);
+assignin('base', 'k', k);
 sim('n.slx');
-cost = ITAE(length(ITAE));
+
+if ~exist('ITAE', 'var')
+    if evalin('base', 'exist(''ITAE'', ''var'')')
+        ITAE = evalin('base', 'ITAE');
+    else
+        error('optimize_PID:MissingITAE', 'The Simulink model did not return an ITAE signal.');
+    end
+end
+
+cost = lastSignalValue(ITAE);
+if ~isfinite(cost)
+    cost = inf;
+end
+end
+
+function value = lastSignalValue(signal)
+if isa(signal, 'timeseries')
+    data = signal.Data;
+elseif isstruct(signal) && isfield(signal, 'signals')
+    data = signal.signals.values;
+else
+    data = signal;
+end
+
+value = data(end);
 end
